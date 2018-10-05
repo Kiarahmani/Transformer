2c0b1cce3fea21eef8b2798ea0884a8af47d859f
Kiarahmani <rahmank@purdue.edu>
2018-10-05 19:19:13 -0400

    sql parser tested -> seems to be fine

--- src/SQLParser.java
+++ src/SQLParser.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 import ir.Table;
@@ -8,16 +14,36 @@ import net.sf.jsqlparser.statement.*;
 import net.sf.jsqlparser.statement.create.table.CreateTable;
 
 public class SQLParser {
-
 	ArrayList<Table> parse() {
-		try {
-			Statement statement = CCJSqlParserUtil.parse("CREATE TABLE ACCOUNT ("
-					+ "    ACC_ID       BIGINT NOT NULL," + "    ACC_NAME     VARCHAR(64) NOT NULL,"
-					+ "    ACC_BAL    	 INT NOT NULL," + "    PRIMARY KEY (ACC_ID)" + ");");
-			CreateTable selectStatement = (CreateTable) statement;
-			System.out.println(selectStatement.getColumnDefinitions());
 
-		} catch (JSQLParserException e) {
+		BufferedReader in = null;
+		ArrayList<String> statements = new ArrayList<String>();
+		try {
+			in = new BufferedReader(new FileReader("src/tests/ddl.sql"));
+			String read = null;
+			String iter_s = "";
+			while ((read = in.readLine()) != null) {
+				iter_s += read;
+				if (read.contains(";")) {
+					statements.add(iter_s);
+					iter_s = "";
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			Statement statement;
+			for (String s:statements) {
+				System.out.println("----------------------");
+				statement = CCJSqlParserUtil.parse(s);
+				CreateTable createStatement = (CreateTable) statement;
+				System.out.println(createStatement.getColumnDefinitions().get(2).getColDataType());
+				System.out.println("----------------------");
+			}
+	
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return null;
