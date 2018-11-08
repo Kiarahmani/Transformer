da994d167757f40c44d7ea6c1d45b7399b3165cf
Kiarahmani <rahmank@purdue.edu>
2018-11-08 10:14:30 -0500

    ddl restructured

---
+++ src/ddl/ddl.sql
@@ -0,0 +1,11 @@
+CREATE TABLE A (
+    id          SMALLINT NOT NULL,
+    balance      SMALLINT  NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE B (
+    id          SMALLINT NOT NULL,
+    name        VARCHAR(32),
+    PRIMARY KEY (id)
+);
