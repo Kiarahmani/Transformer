60f0c1e295839d2fffe8c3e6a709ff7380bb49d9
Kiarahmani <rahmank@purdue.edu>
2018-12-04 14:51:27 -0500

    visualizer fixed

--- src/tests/LostUpdate.java
+++ src/tests/LostUpdate.java
@@ -19,34 +19,7 @@ public class LostUpdate {
 		p.setProperty("ID", String.valueOf(insID));
 	}
 
-	public void updateBalance(int val) throws Exception {
-		try {
-
-			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
-			System.out.println("connecting...");
-			connect = DriverManager.getConnection("jdbc:cassandra://localhost" + ":1904" + insID + "/testks");
-
-			PreparedStatement ps = connect.prepareStatement("select * from A where id=?");
-			ps.setInt(1, 1);
-			ResultSet rs = ps.executeQuery();
-			rs.next();
-
-			ps = connect.prepareStatement("update A set balance = ? where id=?");
-			int balance = rs.getInt("balance");
-			ps.setInt(1, balance + val);
-			ps.setInt(2, 1);
-
-			ps.executeUpdate();
-
-		} catch (Exception e) {
-			throw e;
-		} finally {
-
-		}
-
-	}
-
-	public void updateBalance1(int val) throws Exception {
+	public void readingShit(int val) throws Exception {
 		try {
 
 			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
@@ -60,15 +33,15 @@ public class LostUpdate {
 			int balance = rs.getInt("balance");
 			System.out.println(balance);
 			
-			ps = connect.prepareStatement("select * from A where id=?");
-			ps.setInt(1, 1);
-			rs = ps.executeQuery();
-			rs.next();
-			int balance2 = rs.getInt("balance");
+			PreparedStatement ps2 = connect.prepareStatement("select * from A where id=?");
+			ps2.setInt(1, 1);
+			ResultSet rs2 = ps2.executeQuery();
+			rs2.next();
+			int balance2 = rs2.getInt("balance");
 			System.out.println(balance2);
+			
+			
 
-			
-			
 		} catch (Exception e) {
 			throw e;
 		} finally {
@@ -78,6 +51,26 @@ public class LostUpdate {
 	}
 
 	public void updateBalance2(int val) throws Exception {
+		try {
+
+			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
+			System.out.println("connecting...");
+			connect = DriverManager.getConnection("jdbc:cassandra://localhost" + ":1904" + insID + "/testks");
+
+			PreparedStatement ps = connect.prepareStatement("update A set balance = ? where id=?");
+			ps.setInt(1, val);
+			ps.setInt(2, 1);
+			ps.executeUpdate();
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+
+		}
+
+	}
+	
+	public void updateBalance3(int val) throws Exception {
 		try {
 
 			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
