5069bf349152aa7d199587bc7c4504fdb47f1402
Kiarahmani <rahmank@purdue.edu>
2018-12-04 18:34:25 -0500

    reverted

--- src/tests/LostUpdate.java
+++ src/tests/LostUpdate.java
@@ -30,6 +30,67 @@ public class LostUpdate {
 			ps.setInt(1, 1);
 			ResultSet rs = ps.executeQuery();
 			rs.next();
+<<<<<<< HEAD
+=======
+
+			ps = connect.prepareStatement("update A set balance = ? where id=?");
+			int balance = rs.getInt("balance");
+			ps.setInt(1, balance + val);
+			ps.setInt(2, 1);
+
+			ps.executeUpdate();
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+
+		}
+
+	}
+
+	public void updateBalance1(int val) throws Exception {
+		try {
+
+			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
+			System.out.println("connecting...");
+			connect = DriverManager.getConnection("jdbc:cassandra://localhost" + ":1904" + insID + "/testks");
+
+			PreparedStatement ps = connect.prepareStatement("select * from A where id=?");
+			ps.setInt(1, 1);
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			int balance = rs.getInt("balance");
+			System.out.println(balance);
+			
+			ps = connect.prepareStatement("select * from A where id=?");
+			ps.setInt(1, 1);
+			rs = ps.executeQuery();
+			rs.next();
+			int balance2 = rs.getInt("balance");
+			System.out.println(balance2);
+
+			
+			
+		} catch (Exception e) {
+			throw e;
+		} finally {
+
+		}
+
+	}
+
+	public void updateBalance2(int val) throws Exception {
+		try {
+
+			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
+			System.out.println("connecting...");
+			connect = DriverManager.getConnection("jdbc:cassandra://localhost" + ":1904" + insID + "/testks");
+
+			PreparedStatement ps = connect.prepareStatement("update A set balance = ? where id=?");
+			ps.setInt(1, val);
+			ps.setInt(2, 1);
+			ps.executeUpdate();
+>>>>>>> parent of 60f0c1e... visualizer fixed
 
 			ps = connect.prepareStatement("update A set balance = ? where id=?");
 			int balance = rs.getInt("balance");
